521ac35cd4307bdea8f218f275e5103f2a74370f
rahulgidde <rahulgidde1212@gmail.com>
2020-03-03 19:15:48 +0530

    ADDED:Validate First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "---------------Welcome To User Registration----------------"
+
+function validateFirstName()
+{
+   read -p "Enter First Name : " firstName
+   local pattern="^[A-Z][a-zA-Z]{2,}$"
+   if [[ $firstName =~ $pattern ]]
+   then
+      echo "First name is Valid"
+   else
+      echo "Invalid First Name Enter First Name Start With Cap and Enter minimum 3 Characters"
+   fi
+}
+validateFirstName
+
+
