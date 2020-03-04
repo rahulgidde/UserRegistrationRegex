1dc2efc08b2a6cf866e588502d4be7ad54c4c921
rahulgidde <rahulgidde1212@gmail.com>
2020-03-04 09:05:55 +0530

    ADDED:Validate Last Name Function

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,17 +2,32 @@
 
 echo "---------------Welcome To User Registration----------------"
 
+#FUNCTION FOR VALIDATE FIRST NAME
 function validateFirstName()
 {
-   read -p "Enter First Name : " firstName
+   read -p "Enter First Name: " firstName
    local pattern="^[A-Z][a-zA-Z]{2,}$"
    if [[ $firstName =~ $pattern ]]
    then
-      echo "First name is Valid"
+      echo "First Name is Valid"
    else
       echo "Invalid First Name Enter First Name Start With Cap and Enter minimum 3 Characters"
    fi
 }
+
+#FUNCTION FOR VALIDATE LAST NAME
+function validateLastName()
+{
+	read -p "Enter Last Name: " lastName
+	local pattern="^[A-Z][a-zA-Z]{2,}$"
+	if [[ $lastName =~ $pattern ]]
+	then
+		echo "Last Name is Valid"
+	else
+		echo "Invalid Last Name Enter Last Name Start With Cap and Enter minimum 3 Characters"
+	fi
+}
 validateFirstName
+validateLastName
 
 
